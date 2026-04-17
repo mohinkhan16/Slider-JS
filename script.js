d93766c73510be81f5c30be251f0a496ce58b4f6
MohinKhan <mohinpathan2004@gmail.com>
2026-04-17 08:26:17 +0530

    Refactor image carousel logic in script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const Images=[
-    "https://images.pexels.com/photos/28587130/pexels-photo-28587130.jpeg?_gl=1*4v2a1b*_ga*NDY5MDA4MTExLjE3NTk2NzIxMzg.*_ga_8JE65Q40S6*czE3NzYzMjkzNTgkbzckZzEkdDE3NzYzMjk0MDUkajEzJGwwJGgw",
     "https://images.pexels.com/photos/13348136/pexels-photo-13348136.jpeg?_gl=1*16jlxux*_ga*NDY5MDA4MTExLjE3NTk2NzIxMzg.*_ga_8JE65Q40S6*czE3NzYzMjkzNTgkbzckZzEkdDE3NzYzMjk0NjMkajUzJGwwJGgw",
     "https://images.pexels.com/photos/30299891/pexels-photo-30299891.jpeg?_gl=1*4ub9gk*_ga*NDY5MDA4MTExLjE3NTk2NzIxMzg.*_ga_8JE65Q40S6*czE3NzYzMjkzNTgkbzckZzEkdDE3NzYzMjk1MTckajU5JGwwJGgw",
    "https://images.pexels.com/photos/12735797/pexels-photo-12735797.jpeg?_gl=1*mgcrwc*_ga*NDY5MDA4MTExLjE3NTk2NzIxMzg.*_ga_8JE65Q40S6*czE3NzYzMjkzNTgkbzckZzEkdDE3NzYzMjk1NTIkajI0JGwwJGgw",
